a38036224fc86772ed7f09ecbf1fb7788ef683b9
Daniel Oliveira <danieloliveiradejesus2003@gmail.com>
2025-01-22 14:26:32 -0300

    Lidando com DTO de resposta na consulta por id

--- demo-park-api/src/main/java/com/danieloliveira/demo_park_api/web/controllers/UsuarioController.java
+++ demo-park-api/src/main/java/com/danieloliveira/demo_park_api/web/controllers/UsuarioController.java
@@ -26,9 +26,9 @@ public class UsuarioController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Usuario> getById(@PathVariable Long id) {
+    public ResponseEntity<UsuarioResponseDTO> getById(@PathVariable Long id) {
         Usuario user = usuarioService.buscarPorId(id);
-        return ResponseEntity.ok().body(user);
+        return ResponseEntity.ok().body(UsuarioMapper.toDto(user));
     }
 
     // put é uma atualização completa do objeto, patch um parcial
