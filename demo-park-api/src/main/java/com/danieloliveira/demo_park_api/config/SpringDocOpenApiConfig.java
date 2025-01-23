0ddd8bf1bd73aad6c29da17c3750cddf543c1475
Daniel Oliveira <danieloliveiradejesus2003@gmail.com>
2025-01-23 13:59:54 -0300

    Configurando o SpringDoc OpenAPI 3.1 e Swagger 3

---
+++ demo-park-api/src/main/java/com/danieloliveira/demo_park_api/config/SpringDocOpenApiConfig.java
@@ -0,0 +1,17 @@
+package com.danieloliveira.demo_park_api.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocOpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI().info(new Info().title("REST API - Spring Park").description("Spring Park API Documentation").version("1.0").license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0")).contact(new Contact().name("Daniel Oliveira").email("daniel@spring-park.com")));
+    }
+}
