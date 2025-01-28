ad17ad9d1cab509ccb91e956bcc6cb7104a139c1
Daniel Oliveira <danieloliveiradejesus2003@gmail.com>
2025-01-28 17:18:01 -0300

    Teste ponto a ponto para Create (Status 403, 409 e 422)

--- demo-park-api/src/test/java/com/danieloliveira/demo_park_api/ClienteIT.java
+++ demo-park-api/src/test/java/com/danieloliveira/demo_park_api/ClienteIT.java
@@ -3,6 +3,7 @@ package com.danieloliveira.demo_park_api;
 
 import com.danieloliveira.demo_park_api.web.dto.ClienteCreateDTO;
 import com.danieloliveira.demo_park_api.web.dto.ClienteResponseDTO;
+import com.danieloliveira.demo_park_api.web.exceptions.ErrorMessage;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,16 +20,17 @@ import org.springframework.test.web.reactive.server.WebTestClient;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 
 // essa annotation importa os scripts sql criados e diz quando eles devem ser excutados
-@Sql(scripts = "/clientes/clientes-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 // executa o script de insert antes de executar o teste
 @Sql(scripts = "/clientes/clientes-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 // executa o script de delete após finalizar o teste
+@Sql(scripts = "/clientes/clientes-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class ClienteIT {
 
     @Autowired
     private WebTestClient testClient;
 
     @Test
+    // teste criação de um cliente com os dados todos válidos
     public void CriarCliente_ComDadosValidos_RetornarClienteComStatus201  () {
         ClienteResponseDTO responseBody = testClient
                 .post()
@@ -50,4 +52,101 @@ public class ClienteIT {
         Assertions.assertThat(responseBody.getCpf()).isEqualTo("55222387046");
     }
 
+
+    @Test
+    // teste criando um cliente com um cpf que já está cadastrado
+    public void CriarCliente_ComCpfJaCadastrado_RetornarErrorMessageStatus409  () {
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "toby@gmail.com", "123456"))
+                .bodyValue(new ClienteCreateDTO("Tobias Ferreira", "65456623099"))
+                .exchange()
+                .expectStatus().isEqualTo(409)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(409);
+
+    }
+
+
+    @Test
+    // teste criando um cliente com os dados inválidos
+    public void CriarCliente_ComDadosInvalidos_RetornarErrorMessageStatus422  () {
+        // variação 1: com o nome e cpf vazio
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "toby@gmail.com", "123456"))
+                .bodyValue(new ClienteCreateDTO("", ""))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+
+        // variação 2: com o nome com menos de 5 digitos e com um cpf inválido
+        responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "toby@gmail.com", "123456"))
+                .bodyValue(new ClienteCreateDTO("Toby", "00000000000"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+
+        // variação 3: com o cpf com pontuação (que api não aceita)
+        responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "toby@gmail.com", "123456"))
+                .bodyValue(new ClienteCreateDTO("Toby", "552.223.870-46"))
+                .exchange()
+                .expectStatus().isEqualTo(422)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(422);
+
+    }
+
+
+    @Test
+    // teste criando um cliente com um usuário não autorizado
+    public void CriarCliente_ComUsuarioNaoPermitido_RetornarErrorMessageStatus403  () {
+        ErrorMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/clientes")
+                .contentType(MediaType.APPLICATION_JSON)
+                .headers(JwtAuthentication.getHeaderAuthorization(testClient, "ana@gmail.com", "123456"))
+                .bodyValue(new ClienteCreateDTO("Ana Silva", "55222387046"))
+                .exchange()
+                .expectStatus().isEqualTo(403)
+                .expectBody(ErrorMessage.class)
+                .returnResult().getResponseBody();
+
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.getStatus()).isEqualTo(403);
+
+    }
 }
--- demo-park-api/src/test/sql/clientes/clientes-insert.sql
+++ demo-park-api/src/test/sql/clientes/clientes-insert.sql
@@ -4,5 +4,5 @@ insert into USUARIOS (id, username, password, role)values (102, 'bob@gmail.com',
 insert into USUARIOS (id, username, password, role)values (103, 'toby@gmail.com', '$2a$12$YWGaqAam3xYYecutO98SFu9dQ7aEAQBVSKbnYKadrK4MVBIVA8hkq', 'ROLE_CLIENTE');
 
 
-insert into CLIENTES(id, nome, cpf, id_usuario) values (10, 'João Silva', '54802854080', 101);
-insert into CLIENTES(id, nome, cpf, id_usuario) values (20, 'Roberto Gomes', '20930378075', 102);
+insert into CLIENTES(id, nome, cpf, id_usuario) values (10, 'João Silva', '94234490008', 101);
+insert into CLIENTES(id, nome, cpf, id_usuario) values (20, 'Roberto Gomes', '65456623099', 102);
