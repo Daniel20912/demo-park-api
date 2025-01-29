e5881dfd74d8fe177aa428b2503b0c44280925ce
Daniel Oliveira <danieloliveiradejesus2003@gmail.com>
2025-01-29 18:41:19 -0300

    Realizando o check-in de um veículo via Postman

--- demo-park-api/src/main/java/com/danieloliveira/demo_park_api/web/dto/EstacionamentoResponseDTO.java
+++ demo-park-api/src/main/java/com/danieloliveira/demo_park_api/web/dto/EstacionamentoResponseDTO.java
@@ -1,5 +1,9 @@
 package com.danieloliveira.demo_park_api.web.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,6 +16,7 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL) // assim os campos de respostas que estiverem nulos não iram aparecer no corpo de resposta
 public class EstacionamentoResponseDTO {
 
     private String placa;
@@ -21,8 +26,13 @@ public class EstacionamentoResponseDTO {
     private String clienteCpf;
 
     private String recibo;
+
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
     private LocalDateTime dataEntrada;
+
+    @JsonFormat(pattern = "yyyy-MM-dd hh:mm:ss")
     private LocalDateTime dataSaida;
+
     private String vagaCodigo;
     private BigDecimal valor;
     private BigDecimal desconto;
